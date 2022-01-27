75fac62965f91150115418c19de6815c0a0e47dc
sam-klok <sklok11@gmail.com>
2022-01-27 10:04:36 -0700

    Added screen shot

--- WebAppGrid/Controllers/HomeController.cs
+++ WebAppGrid/Controllers/HomeController.cs
@@ -16,15 +16,15 @@ namespace WebAppGrid.Controllers
             people = new List<Person>() 
             {
                 new Person("Sam Klok",      30,DateTime.Parse("01/01/1980"),true),
-                new Person("Tristan Klok",  10,DateTime.Parse("02/01/2010"),false),
-                new Person("McKayla Klok",  09,DateTime.Parse("03/01/2012"),false),
-                new Person("Anastasia Klok",20,DateTime.Parse("04/01/1980"),false),
-                new Person("Amanda Klok",   25,DateTime.Parse("05/07/1980"),true),
-                new Person("Dedushak Klok", 80,DateTime.Parse("05/16/1980"),true),
-                new Person("Babushk Klok",  81,DateTime.Parse("08/01/1980"),true),
+                new Person("Tristan",  10,DateTime.Parse("02/01/2010"),false),
+                new Person("McKayla",  09,DateTime.Parse("03/01/2012"),false),
+                new Person("Anastasia",20,DateTime.Parse("04/01/1980"),false),
+                new Person("Amanda",   25,DateTime.Parse("05/07/1980"),true),
+                new Person("Dedushka", 80,DateTime.Parse("05/16/1980"),true),
+                new Person("Babushka",  81,DateTime.Parse("08/01/1980"),true),
                 new Person("John Smith",    54,DateTime.Parse("12/01/1980"),false),
                 new Person("Joseph Boles",  65,DateTime.Parse("11/21/1980"),false),
-                new Person("Priyanka Agrwl",33,DateTime.Parse("01/01/1980"),false),
+                new Person("Priyanka Agrwal",33,DateTime.Parse("01/01/1980"),false),
             };
         }
 
