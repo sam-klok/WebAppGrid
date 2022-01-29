18629e4e3cd743493ea84251a770c311c75662e7
sam-klok <sklok11@gmail.com>
2022-01-29 12:45:47 -0700

    Adding JavaScript closure

--- WebAppGrid/Scripts/Home/index.js
+++ WebAppGrid/Scripts/Home/index.js
@@ -1,11 +1,29 @@
-﻿function selectedCheckbox(a, b){
-    if (a.checked) {
-        localStorage.setItem(b,'checked');
-    }
-    else {
-        localStorage.removeItem(b);
-    }
+﻿var pageIndex = (function () {
+    function toStorage(a, b) {
+        var key = "cbxToDeleteId=" + b;
+        if (a.checked) {
+            localStorage.setItem(key, 'checked');
+        }
+        else {
+            localStorage.removeItem(key);
+        }
 
-    //console.log(a);  // a = this
-    //console.log(b);
-}
+        //console.log(a);  // a = this
+        //console.log(b);
+    };
+
+    function testLocal() {
+        console.log("testLocal() - called");
+    };
+
+    return {
+        Init: function () {
+            console.log("pageIndex.Init() - called");
+        },
+        toStorage   // making it public
+    };
+})(); // "dog balls"...
+
+$(document).ready(function () {
+    pageIndex.Init();
+});
