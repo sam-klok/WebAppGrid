bc248716e0d5be376d31bf7360949acfa8469ca0
sam-klok <sklok11@gmail.com>
2022-01-29 13:41:54 -0700

    Show checked list alert

--- WebAppGrid/Scripts/Home/index.js
+++ WebAppGrid/Scripts/Home/index.js
@@ -12,6 +12,7 @@
         //console.log(b);
     };
 
+    // reading LocalStorage and checking checkboxes
     function setCheckboxesFromStorage() {
         console.log("setCheckboxesFromStorage() - calling");
         keys = Object.keys(localStorage);
@@ -30,12 +31,32 @@
         console.log("testLocal() - called");
     };
 
+    function showCheckedRecords() {
+        keys = Object.keys(localStorage);
+        i = keys.length;
+        var list = "";
+
+        while (i--) {
+            var checkBoxId = keys[i];
+
+            if (checkBoxId.indexOf('cbxToDelete') >= 0) {
+                list += checkBoxId.replace('cbxToDelete', '') + ' ';
+            }
+        }
+
+        if (list == "")
+            alert('Nothing checked.');
+        else
+            alert("Selected IDs: " + list);
+    }
+
     return {
         Init: function () {
             //console.log("pageIndex.Init() - called");
             setCheckboxesFromStorage();
         },
         toStorage,   // making it public
+        showCheckedRecords,
     };
 })(); // "dog balls"...
 
