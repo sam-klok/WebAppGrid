d69a1f6837cecae1eca12bc584387e0045aa746d
sam-klok <sklok11@gmail.com>
2022-03-05 11:38:28 -0700

    Refactoring code to remove hardcode, and improving logic around Select All checkbox

--- WebAppGrid/Scripts/Home/pageIndex.js
+++ WebAppGrid/Scripts/Home/pageIndex.js
@@ -1,11 +1,15 @@
 ﻿var pageIndex = (function () {
+    var keySelectAll = "cbxSelectAll";
+    var keyToDelete = "cbxToDelete";
+    var sChecked = "checked";
+
     function toStorage(a, b) {
-        var key = "cbxToDelete" + b;
+        var k = keyToDelete + b;
         if (a.checked) {
-            localStorage.setItem(key, 'checked');
+            localStorage.setItem(k, sChecked);
         }
         else {
-            localStorage.removeItem(key);
+            localStorage.removeItem(k);
         }
         //console.log(a);  // a = this
     };
@@ -13,14 +17,16 @@
     // reading LocalStorage and checking checkboxes
     function setCheckboxesFromStorage() {
         console.log("setCheckboxesFromStorage() - calling");
+
         keys = Object.keys(localStorage);
         i = keys.length;
 
         while (i--) {
             var checkBoxId = keys[i];
 
-            if (checkBoxId.indexOf("cbxToDelete") >= 0) {
-                $("#" + checkBoxId).prop('checked', true);
+            // looking for normal or 'Select All' checkboxes in the storage
+            if (checkBoxId.indexOf(keyToDelete) >= 0 || checkBoxId.indexOf(keySelectAll) >= 0) {
+                $("#" + checkBoxId).prop(sChecked, true);
             }
         }
     }
@@ -37,8 +43,8 @@
         while (i--) {
             var checkBoxId = keys[i];
 
-            if (checkBoxId.indexOf('cbxToDelete') >= 0) {
-                list += checkBoxId.replace('cbxToDelete', '') + ' ';
+            if (checkBoxId.indexOf(keyToDelete) >= 0) {
+                list += checkBoxId.replace(keyToDelete, '') + ' ';
             }
         }
 
@@ -53,14 +59,16 @@
             console.log("check box " + id + " clicked.");
 
             var id = $(this).attr("value");
-            var key = "cbxToDelete" + id;
+            var k = keyToDelete + id;
 
             if ($(this)[0].checked) {
-                localStorage.setItem(key, 'checked');
+                localStorage.setItem(k, sChecked);
             }
             else {
-                localStorage.removeItem(key);
-                $('#cbxSelectAll').prop('checked', false); // uncheck All checkbox
+                localStorage.removeItem(k);
+
+                localStorage.removeItem(keySelectAll);
+                $('#' + keySelectAll).prop(sChecked, false); // uncheck 'Select All' checkbox
             }
 
             //showCheckedRecords();
@@ -71,27 +79,25 @@
         $('#cbxSelectAll').click(function () {
             console.log("Checkbox Select All clicked.");
 
-            var keyAll = "cbxSelectAll";
-
             if ($(this)[0].checked) {
-                localStorage.setItem(keyAll, 'checked');
+                localStorage.setItem(keySelectAll, sChecked);
 
                 for (var i = 0; i < model.length - 1; i++) {
-                    key = "cbxToDelete" + model[i].Id;
-                    localStorage.setItem(key, 'checked');
+                    k = keyToDelete + model[i].Id;
+                    localStorage.setItem(k, sChecked);
                 }
 
-                $(".cbToDel").prop('checked', true);
+                $(".cbToDel").prop(sChecked, true);
             }
             else {
-                localStorage.removeItem(keyAll);
+                localStorage.removeItem(keySelectAll);
 
                 for (var i = 0; i < model.length - 1; i++) {
-                    key = "cbxToDelete" + model[i].Id;
-                    localStorage.removeItem(key);
+                    k = keyToDelete + model[i].Id;
+                    localStorage.removeItem(k);
                 }
 
-                $(".cbToDel").prop('checked', false);
+                $(".cbToDel").prop(sChecked, false);
             }
         });
     };
