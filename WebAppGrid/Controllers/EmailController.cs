d4ed3a40d8020a9d8ab2fa5c3a910be2cf809ffc
sam-klok <sklok11@gmail.com>
2023-06-05 21:36:18 -0700

    Using Session to store data..

--- WebAppGrid/Controllers/EmailController.cs
+++ WebAppGrid/Controllers/EmailController.cs
@@ -10,23 +10,31 @@ namespace WebAppGrid.Controllers
 {
     public class EmailController : Controller
     {
-        public List<EmailAddress> distributionList;
+        //public List<EmailAddress> distributionList;
 
         public EmailController() {
-            distributionList = new List<EmailAddress>()
+            if (System.Web.HttpContext.Current.Session["DistributionList"] == null)
             {
-                new EmailAddress(1,"sklok11@gmail.com", true, "Serge", "Klokov"),
-                new EmailAddress(2,"dune@dog.org"),
-            };
+                var distributionList = new List<EmailAddress>()
+                {
+                    new EmailAddress(1,"sklok11@gmail.com", true, "Serge", "Klokov"),
+                    new EmailAddress(2,"dune@dog.org"),
+                };
+
+                System.Web.HttpContext.Current.Session["DistributionList"] = distributionList;
+            }
         }
 
         public ActionResult Index()
         {
-            return View(distributionList);
+            List<EmailAddress> dl = (List<EmailAddress>)Session["DistributionList"];
+
+            return View(dl);
         }
 
         public ActionResult Edit(int id) {
-            EmailAddress model = distributionList.FirstOrDefault(x => x.ID == id);
+            List<EmailAddress> dl = (List<EmailAddress>)Session["DistributionList"];
+            EmailAddress model = dl.FirstOrDefault(x => x.ID == id);
 
             return View(model);
         }
@@ -34,12 +42,16 @@ namespace WebAppGrid.Controllers
         [HttpPost]
         public ActionResult Edit(EmailAddress model)
         {
-            var m = distributionList.FirstOrDefault(x => x.ID == model.ID);
+            List<EmailAddress> dl = (List<EmailAddress>)Session["DistributionList"];
+            EmailAddress m = dl.FirstOrDefault(x => x.ID == model.ID);
+
             m.Email = model.Email;
             m.LastName = model.LastName;
             m.FirstName = model.FirstName;
             m.Active= model.Active;
             m.Comment= model.Comment;
+
+            Session["DistributionList"] = dl;
 
             return RedirectToAction("Index");
             //return RedirectToAction("Index", distributionList);
