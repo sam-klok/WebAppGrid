0c6cf9e4a6b7ff76702e92eb3a4f5de8fbb924cc
sam-klok <sklok11@gmail.com>
2023-06-05 19:12:08 -0700

    add grid with email distribution list

---
+++ WebAppGrid/Controllers/EmailController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebAppGrid.Models;
+
+namespace WebAppGrid.Controllers
+{
+    public class EmailController : Controller
+    {
+        public List<EmailAddress> distributionList;
+
+        public EmailController() {
+            distributionList = new List<EmailAddress>()
+            {
+                new EmailAddress("sklok11@gmail.com", true, "Serge", "Klokov"),
+                new EmailAddress("dune@dog.org"),
+            };
+        }
+
+        public ActionResult Index()
+        {
+            return View(distributionList);
+        }
+    }
+}
---
+++ WebAppGrid/Models/EmailAddress.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAppGrid.Models
+{
+    public class EmailAddress
+    {
+        public string Email { get; set; }
+        public bool Active { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Comment { get; set; }
+
+        public EmailAddress(string email, bool active = true, string firstName = null, string lastName = null, string comment = null)
+        {
+            Email = email;
+            Active = active;
+            FirstName = firstName;
+            LastName = lastName;
+            Comment = comment;
+        }
+    }
+}
