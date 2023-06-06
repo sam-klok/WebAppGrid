187ce0ecb61bb9d1e73875c9fc1790a06902f573
sam-klok <sklok11@gmail.com>
2023-06-05 21:24:03 -0700

    editing in mvc grid

--- WebAppGrid/Controllers/EmailController.cs
+++ WebAppGrid/Controllers/EmailController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Web;
 using System.Web.Mvc;
 using WebAppGrid.Models;
@@ -14,8 +15,8 @@ namespace WebAppGrid.Controllers
         public EmailController() {
             distributionList = new List<EmailAddress>()
             {
-                new EmailAddress("sklok11@gmail.com", true, "Serge", "Klokov"),
-                new EmailAddress("dune@dog.org"),
+                new EmailAddress(1,"sklok11@gmail.com", true, "Serge", "Klokov"),
+                new EmailAddress(2,"dune@dog.org"),
             };
         }
 
@@ -23,5 +24,27 @@ namespace WebAppGrid.Controllers
         {
             return View(distributionList);
         }
+
+        public ActionResult Edit(int id) {
+            EmailAddress model = distributionList.FirstOrDefault(x => x.ID == id);
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(EmailAddress model)
+        {
+            var m = distributionList.FirstOrDefault(x => x.ID == model.ID);
+            m.Email = model.Email;
+            m.LastName = model.LastName;
+            m.FirstName = model.FirstName;
+            m.Active= model.Active;
+            m.Comment= model.Comment;
+
+            return RedirectToAction("Index");
+            //return RedirectToAction("Index", distributionList);
+        }
+
     }
+
 }
--- WebAppGrid/Models/EmailAddress.cs
+++ WebAppGrid/Models/EmailAddress.cs
@@ -7,14 +7,20 @@ namespace WebAppGrid.Models
 {
     public class EmailAddress
     {
+        public int ID { get; set; }
         public string Email { get; set; }
         public bool Active { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Comment { get; set; }
 
-        public EmailAddress(string email, bool active = true, string firstName = null, string lastName = null, string comment = null)
+        public EmailAddress()
+        { 
+        }
+
+        public EmailAddress(int id, string email, bool active = true, string firstName = null, string lastName = null, string comment = null)
         {
+            ID = id;
             Email = email;
             Active = active;
             FirstName = firstName;
